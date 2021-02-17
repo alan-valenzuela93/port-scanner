76d3256881dea461034d70981f711ae5a0943db5
Alan Valenzuela <61110391+alan-valenzuela93@users.noreply.github.com>
2021-02-16 23:56:16 -0300

    banner grabber added

--- port-scanner.py
+++ port-scanner.py
@@ -1,39 +1,34 @@
 import socket
 import argparse
+from grabber import banner_grabbing
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--target', help='Enter your target address', required=True)
+parser = parser.parse_args()
+ports = [21, 22, 25, 53, 66, 80, 88, 110, 139, 443, 445, 8080, 9050]  # These are some of the most interesting ports to scan
+
+
+def get_ip(target):
+    return str(socket.gethostbyname(target))
+
 
 def scan(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host, port))
+        s.settimeout(0.2)  # Increase scanning speed
+    except:
+        return False
+    else:
+        return True
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	try:
-		s.connect((host, port))
-		s.settimeout(0.2) #Increase scanning speed
-	except:
-		return False
-	else:
-		return True
 
 def main():
+    for p in ports:
+        if scan(parser.target, p):
+            print(banner_grabbing(parser.target, p))
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-t', '--target', help='Enter your target adress')
-	parser = parser.parse_args()
-	ports = [21, 22, 25, 53, 66, 80, 88, 110, 443, 8080, 9050] #This are some of the most interesting ports to scan
-	result = dict() #I save the result as a dictionary, with the port as the key and the status as it's value.
-
-	for p in ports:
-		if(scan(parser.target, p)):
-			result.setdefault(p, 'OPEN')
-		else:
-			result.setdefault(p, 'CLOSE')
-
-	print('')
-	print('PORT | STATUS')
-	print('')
-	
-	for key, value in result.items():
-		print(key, '   ', value)
 
 if __name__ == '__main__':
-	print('Scanning started...')
-	print('')
-	main()
+    print('TCP/IP scan started at IP ' + get_ip(parser.target))
+    main()
